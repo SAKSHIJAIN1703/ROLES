b129b794f09cb66926909e3fb9aaf2f9a93ba580
sj96715 <sj96715@Siddharth.com>
2021-10-29 19:03:43 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+<title>table</title>
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+<div class="table-container">
+<h1 class="heading">Roles</h1>
+<table class="table">
+
+<tr>
+<td>consultant</td>
+</tr>
+<tr>
+<td>employee</td>
+</tr>
+
+</body>
+</html>
